f3a780df1b092d928769d679aac68a9dc78008fa
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-07-23 11:34:44 -0600

    Implementación de GeneracionCAI.js y db actualizada

--- src/app.js
+++ src/app.js
@@ -6,39 +6,46 @@ const data = fs.readFileSync('db.json', 'utf8');
 const db = JSON.parse(data)
 const Empleados = db.Empleados
 const Productos = db.Productos
-
+const Sucursales= db.Sucursales
 
 //primera factura
 let numFactura = 1
 let numCajero = 0
-let sucursal = 'Los Angeles'
+let numSucursal= 0
+
 const porcentajeISV = 0.15
 const cajeroData = Empleados.Cajero[numCajero]
+
+let sucursalData= Sucursales[numSucursal]
 let productComprados = [Productos[0], Productos[1], Productos[2]]
 
-let datosProductos = ArticulosComprados(productComprados, porcentajeISV)
+let datosProductos = ArticulosComprados(productComprados, porcentajeISV) //retorna productos, subtotal e isv
 
-Factura(numFactura, sucursal, cajeroData, datosProductos)
+Factura(numFactura, sucursalData, cajeroData, datosProductos)
 
 //segunda factura
 
 numFactura++
 numCajero = 2
-sucursal = 'Los Angeles'
+numSucursal= 1
+
+sucursalData = Sucursales[numSucursal]
 productComprados = [Productos[1], Productos[2]]
 
 datosProductos = ArticulosComprados(productComprados, porcentajeISV)
 
-Factura(numFactura, sucursal, cajeroData, datosProductos)
+Factura(numFactura, sucursalData, cajeroData, datosProductos)
 
 //tercera factura
 
 numFactura++
 numCajero = 1
-sucursal = 'Miraflores'
+numSucursal= 0
+
+sucursalData= Sucursales[numSucursal]
 productComprados = [Productos[1]]
 
 datosProductos = ArticulosComprados(productComprados, porcentajeISV)
 
-Factura(numFactura, sucursal, cajeroData, datosProductos)
+Factura(numFactura, sucursalData, cajeroData, datosProductos)
 
--- src/modules/Factura.js
+++ src/modules/Factura.js
@@ -1,15 +1,29 @@
-export const Factura = (numFactura, sucursal, cajero, datosProductos) => {
+import { GeneracionCAI } from "./GeneracionCAI.js"
+
+export const Factura = (numFactura, sucursalData, cajero, datosProductos) => {
 
     const productos = datosProductos.productos
     const total = parseFloat(datosProductos.total)
     const ISV = parseFloat(datosProductos.ISV)
     const totalPagar = total + ISV
 
+    /*
+    CAI:
+    AAA-BBB-CC-DDDDDDDD
+    
+    -AAA: Establecimiento
+    -BBB: Punto de emisión del Establecimiento (se genera mediante Math.random para definir si indice 0 o 1 del arreglo de puntos)
+    -CC: Tipo de documento a generar
+    -DDDDDDDD: Correlativo (8 digitos)
+    */
+    const caiFactura= GeneracionCAI(sucursalData.idEstablecimiento, sucursalData.puntosEmision[Math.floor(Math.random() * 2)], "32")
+
     console.log(
         `
                             EXPRESSO
 
-    Factura #${numFactura}                      Sucursal ${sucursal}
+    CAI: ${caiFactura}
+    Factura #${numFactura}                      Sucursal ${sucursalData.nombreEstablecimiento}
     Fecha: ${Date()}
 
     Cajero: ${cajero.nombre}        ID: ${cajero.id}
@@ -19,7 +33,7 @@ export const Factura = (numFactura, sucursal, cajero, datosProductos) => {
             return `${producto}`;
         }).join('\n    ')}
     ---------------------------------------------------
-    Total                                ${total}
+    Subtotal                                ${total}
     ISV                                  ${ISV.toFixed(2)}
     ---------------------------------------------------
     Total a Pagar                        ${totalPagar}
